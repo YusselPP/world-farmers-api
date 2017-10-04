1407b4c806b8b55fd3a982f860be6107d0391e99
Yussel Paredes <yparedes@tibco-support.com>
2017-10-04 11:12:09 -0500

    Removed users routes

--- routes/api.php
+++ routes/api.php
@@ -13,23 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::post('/user', function (Request $request) {
-	// dd(config('passport.client_secret'));
-    $user = new \App\User;
-    $user->name = 'Y Paredes';
-    $user->password = Hash::make('123admin');
-    $user->email = 'yparedes@gmail.com';
-    $user->scopes = json_encode(['write-contacts']);
-    $user->save();
-    // dd(Hash::check('', '$2y$10$K7KoXmNv.IXnaEUgg/chIuEeM5HIoyEot13HeAuygRe6LH4tpTyd2'));
-});
-
-
-
 // Auth
 Route::post('/login', 'AuthController@login');
 Route::middleware('auth:api')->post('/logout', 'AuthController@logout');
